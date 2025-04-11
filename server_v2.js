34c11db54f1714429193483655b3e17f55bad5f1
KgosiMar <kgosi.maruping17@gmail.com>
2025-04-11 22:02:48 +0200

    Add server_v2.js 1744401767674

---
+++ server_v2.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/api/djs', (req, res) => {
+    fs.readFile(path.join(__dirname, 'data.json'), 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).json({ error: 'Failed to read data' });
+        }
+        res.json(JSON.parse(data));
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
