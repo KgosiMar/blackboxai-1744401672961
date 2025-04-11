3c97275ac7eb18016ea6f488cb8de8d683630077
KgosiMar <kgosi.maruping17@gmail.com>
2025-04-11 22:01:47 +0200

    Add proposal_templates.js 1744401706793

---
+++ proposal_templates.js
@@ -0,0 +1,56 @@
+const proposalTemplates = [
+    {
+        title: "Event Proposal Template",
+        content: `
+        [Your Name]
+        [Your Address]
+        [City, State, Zip Code]
+        [Email Address]
+        [Phone Number]
+        
+        [Date]
+        
+        [Client's Name]
+        [Client's Address]
+        [City, State, Zip Code]
+        
+        Dear [Client's Name],
+        
+        I am writing to propose my services as a DJ for [Event Name] on [Event Date]. I have extensive experience in [Genre/Style] and have performed at various events including [List of Events].
+        
+        My proposed fee for this event is [Fee]. This includes [List of Services Included].
+        
+        I look forward to the opportunity to work together.
+        
+        Sincerely,
+        [Your Name]
+        `
+    },
+    {
+        title: "Collaboration Proposal Template",
+        content: `
+        [Your Name]
+        [Your Address]
+        [City, State, Zip Code]
+        [Email Address]
+        [Phone Number]
+        
+        [Date]
+        
+        [Collaborator's Name]
+        [Collaborator's Address]
+        [City, State, Zip Code]
+        
+        Dear [Collaborator's Name],
+        
+        I hope this message finds you well. I am reaching out to propose a collaboration for [Project/Event Name]. I believe that our combined talents could create something truly special.
+        
+        I would love to discuss this further and explore how we can work together.
+        
+        Best regards,
+        [Your Name]
+        `
+    }
+];
+
+module.exports = proposalTemplates;
